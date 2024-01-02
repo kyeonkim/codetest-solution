8fd968da8a8f95334155299bb8f4d48e05af9844
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-01-02 20:08:43 +0900

    Update 문자열 압축.java
    - 정규식이 아니라 substring 을 이용하여 진행하였다. 정규식은 실제 사용하려고 할 때 사용하기 힘들다는 생각이 들었다.
    - 주기적으로 변경되는 부분에 StringBuilder 를 사용 > java 에서는 String 객체의 값이 변경될 때 바로 이전에 주소에서 변경되는게 아니라 이전 주소를 해제하고 새로운 주소에 할당하면서 변경이 진행된다. 주기적으로 변경되는 변수의 경우에는 상당히 비효율적이므로 이를 효율적으로 하기 위해 StringBuilder를 이용하였다. StringBuilder는 새로운 객체를 생성하는 것이 아니라 기존 객체에 데이터를 더할 수 있다.

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -3,25 +3,29 @@ import java.util.Arrays;
 class Solution {
     public int solution(String s) {
         int min = s.length();
-        int maxCompressLength = s.length() / 2;
+        int sLen = s.length();
+        StringBuilder sb = new StringBuilder(s);
         
-        for (int i = maxCompressLength; i >= 1; i--){
-            String[] subStringArray = s.split("(?<=\\G.{" + i + "})");
-            String equalsStr = subStringArray[0];
-            String compressStr = "";
-            int compressNum = 1;
-            for (int j = 1; j < subStringArray.length; j++){
-                if (!equalsStr.equals(subStringArray[j])){
-                    compressStr += compressNum == 1 ? equalsStr : Integer.toString(compressNum) + equalsStr;
-                    equalsStr = subStringArray[j];
-                    compressNum = 1;
-                    continue;
+        for (int i = 1; i <= sLen / 2; ++i){
+            String prev = "";
+            String cur = "";
+            StringBuilder result = new StringBuilder();
+            int cnt = 1;
+            for (int j = 0; j < sLen; j += i){
+                int end = j + i;
+                if (end > sLen) end = sLen;
+                cur = sb.substring(j, end);
+                if (cur.equals(prev))
+                    cnt++;
+                else if (!prev.isEmpty()){
+                    result.append(cnt == 1 ? prev : Integer.toString(cnt) + prev);
+                    cnt = 1;
                 }
-                compressNum++;
+                prev = cur;
             }
-            compressStr += compressNum == 1 ? equalsStr : Integer.toString(compressNum) + equalsStr;
-            if (min > compressStr.length())
-                min = compressStr.length();
+            result.append(cnt == 1 ? prev : Integer.toString(cnt) + prev);
+            if (min > result.length())
+                min = result.length();
         }
         return min;
     }
