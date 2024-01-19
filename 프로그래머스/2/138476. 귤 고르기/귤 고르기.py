9e499b1ce9e06339168c4fe710aed45f31b62f8e
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-01-19 17:32:37 +0900

    Update 귤 고르기.py

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,15 +1,14 @@
+from collections import defaultdict
+
 def solution(k, tangerine):
     answer = 0
-    tangerineDict = {}
+    tangerineDict = defaultdict(int)
     for t in tangerine:
-        if t in tangerineDict:
-            tangerineDict[t] += 1 
-        else:
-            tangerineDict[t] = 1
+        tangerineDict[t] += 1 
     sortedTangerine = sorted(tangerineDict.items(), key=lambda item: item[1], reverse=True)
-    for value in sortedTangerine:
-        k -= value[1]
+    for _, value in sortedTangerine:
+        k -= value
         answer += 1
         if k <= 0:
-            return answer
+            break
     return answer
