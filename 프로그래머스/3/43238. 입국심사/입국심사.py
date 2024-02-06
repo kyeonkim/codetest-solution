5d8decee24048a629677174903462c089ad78e53
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-02-06 23:54:04 +0900

    [level 3] Title: 입국심사, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,16 @@
+def solution(n, times):
+    answer = 0
+    start = 0
+    end = max(times) * n
+    
+    while start <= end:
+        mid = (start + end) // 2
+        total = 0
+        for time in times:
+            total += mid // time
+        if n <= total:
+            answer = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+    return answer
